a46e57995564a7a7e5d6276a3eccd874347c8ad2
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2019-05-13 08:58:22 +0530

    working on event handling

--- Calcultor.py
+++ Calcultor.py
@@ -5,15 +5,38 @@ root.config(bg='#2b2b2b')
 root.geometry("475x288")
 root.title('Calculator')
 
-Output_frame = Text(root, bg='#2b2b2b', fg='white')
-Output_frame.config(highlightbackground='gray33', highlightthickness=1)
-Output_frame.place(x=4, y=4, width=467, height=80)
+Output_screen_value = StringVar()
+Output_screen_value.set("")
+
+def resolve_event(event):
+    global Output_screen_value
+    global Output_screen
+    text = event.widget.cget('text')
+    if text=='C':
+        Output_screen_value.set("")
+    elif text=='=':
+        result = eval(Output_screen_value.get())
+        Output_screen_value.set(result)
+    elif text=='Backspace':
+        if len(Output_screen_value.get())!=0:
+            value = Output_screen_value.get()
+            value = value[0:len(value)-1]
+            Output_screen_value.set(value)
+    else:
+        Output_screen_value.set(Output_screen_value.get()+text)
+
+
+#-------------------------- Main Screen Creation ------------------------------
+Output_screen = Entry(root, bg='#2b2b2b', disabledbackground='#2b2b2b', foreground='white', textvar=Output_screen_value, justify=RIGHT)
+Output_screen.config(font=('calibari', 12), state=DISABLED)
+Output_screen.config(highlightbackground='gray33', highlightthickness=1)
+Output_screen.place(x=4, y=4, width=467, height=80)
 Date_cal_frame = Frame(root, bg='#2b2b2b', highlightbackground='gray33', highlightthickness=1)
 Date_cal_frame.place(x=171, y=87, width = 300, height=197)
 Date_label = Label(Date_cal_frame, bg='#3c3f41', text='Date Calculation', fg='#BBBBBB')
 Date_label.pack(fill=X)
 
-
+#-------------------------- Date Calculation Graphic ------------------------------
 Date_from_label = Label(Date_cal_frame, text='From', fg='#BBBBBB', bg='#2b2b2b')
 Date_to_label = Label(Date_cal_frame, text='To', fg='#BBBBBB', bg='#2b2b2b')
 Date_from = Entry(Date_cal_frame, fg='white', bg='#2b2b2b', highlightbackground='gray33', highlightthickness=1)
@@ -34,24 +57,60 @@ Result1.place(x=13, y=87, width=278)
 Result2.place(x=13, y=137, width=278)
 Calculate.place(x=220, y=165, width=71, height=23)
 
+#-------------------------- Calculator Graphic ------------------------------
 backspace = Button(root, bg='#2b2b2b', fg='white', text='Backspace')
+backspace.bind("<Button-1>", resolve_event)
+
 clear = b1 = Button(root, bg='#2b2b2b', fg='white', text='C')
+clear.bind("<Button-1>", resolve_event)
+
 b1 = Button(root, bg='#2b2b2b', fg='white', text='1')
+b1.bind("<Button-1>", resolve_event)
+
 b2 = Button(root, bg='#2b2b2b', fg='white', text='2')
+b2.bind("<Button-1>", resolve_event)
+
 b3 = Button(root, bg='#2b2b2b', fg='white', text='3')
+b3.bind("<Button-1>", resolve_event)
+
 b4 = Button(root, bg='#2b2b2b', fg='white', text='4')
+b4.bind("<Button-1>", resolve_event)
+
 b5 = Button(root, bg='#2b2b2b', fg='white', text='5')
+b5.bind("<Button-1>", resolve_event)
+
 b6 = Button(root, bg='#2b2b2b', fg='white', text='6')
+b6.bind("<Button-1>", resolve_event)
+
 b7 = Button(root, bg='#2b2b2b', fg='white', text='7')
+b7.bind("<Button-1>", resolve_event)
+
 b8 = Button(root, bg='#2b2b2b', fg='white', text='8')
+b8.bind("<Button-1>", resolve_event)
+
 b9 = Button(root, bg='#2b2b2b', fg='white', text='9')
+b9.bind("<Button-1>", resolve_event)
+
 b0 = Button(root, bg='#2b2b2b', fg='white', text='0')
+b0.bind("<Button-1>", resolve_event)
+
 point = Button(root, bg='#2b2b2b', fg='white', text='.')
+point.bind("<Button-1>", resolve_event)
+
 equla = Button(root, bg='#2b2b2b', fg='white', text='=')
+equla.bind("<Button-1>", resolve_event)
+
 devide = Button(root, bg='#2b2b2b', fg='white', text='/')
+devide.bind("<Button-1>", resolve_event)
+
 multiply = Button(root, bg='#2b2b2b', fg='white', text='*')
+multiply.bind("<Button-1>", resolve_event)
+
 minus = Button(root, bg='#2b2b2b', fg='white', text='-')
+minus.bind("<Button-1>", resolve_event)
+
 plus = Button(root, bg='#2b2b2b', fg='white', text='+')
+plus.bind("<Button-1>", resolve_event)
 
 backspace.place(x=4, y=87, height=35, width=80)
 clear.place(x=89, y=87, width=35, height=35)
